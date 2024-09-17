8a64927c6a433ab0b3703295ac8f528b5f092f5c
Keegan <62590150+kcolter@users.noreply.github.com>
2024-09-17 09:38:49 -0300

    copied over multer code from slides/follow-along

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import contactsRouter from './routes/contacts.js';
 
+
+
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -8,8 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api/contacts', contactsRouter);
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,6 +2,23 @@ import express from 'express';
 
 const router = express.Router();
 
+// Multer setup
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/'); //save uploaded files in `public/images` folder
+    },
+    filename: function (req, file, cb) {
+        const ext = file.originalname.split('.').pop(); // get file extension
+        const uniqueFilename = Date.now() + '-' + Math.round(Math.random() * 1000) + '.' + ext; //generate unique filename - current timestamp + random number between 0 and 1000.
+        cb(null, uniqueFilename);
+    }
+
+});
+const upload = multer({ storage: storage });
+
+//////////
+//ROUTES//
+//////////
 router.get('/', (req, res) => {
     res.send('Contacts route');
 });
